b28ad5188169b03f7b4212bc2f607f169cf22519
Filipe Cabecinhas <filcab@Farnsworth.lan>
2009-03-07 13:35:12 +0000

    ch03 stuff

--- ch03/BookStore.hs
+++ ch03/BookStore.hs
@@ -2,3 +2,24 @@
 data BookInfo = Book Int String [String]
                 deriving (Show)
 
+data MagazineInfo = Magazine Int String [String]
+                    deriving (Show)
+
+
+--data BookReview = BookReview BookInfo CustomerID String
+type CustomerID = Int
+type ReviewBody = String
+
+data BookReview = BookReview BookInfo CustomerID ReviewBody
+type BookRecord = (BookInfo, BookReview)
+
+
+type CardHolder = String
+type CardNumber = String
+type Address = [String]
+
+data BillingInfo = CreditCard CardNumber CardHolder Address
+                 | CashOnDelivery
+                 | Invoice CustomerID
+                   deriving (Show)
+
